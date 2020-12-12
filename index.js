ceedc86e94a23d805543cc86ebd123fac9483863
Shishir Jha <shishirjhaccr@gmail.com>
2020-12-12 14:06:42 +0530

    initial express setup and add dependencies

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Start your es6 scripts here
+import express from 'express';
+
+const app = express()
+
+// a bare minimum express get request
+app.get('/', (req,res) => {
+    res.send("Up and running with graphql")
+});
+
+// determine the port to listen to
+app.listen(8000, () => console.log("Running at 8000"));
